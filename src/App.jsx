5eb749e253ae43c5c51c63acd0bdabf003127491
anie08 <azayan1903@icloud.com>
2026-09-02 16:58:48 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import Randomiser from "./Spinora2/Spinora.jsx";
+
+function App() {
+
+  return (
+    <div>
+<Randomiser/>
+    </div>
+  )
+}
+
+export default App
